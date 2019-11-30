62c05444ce57c5cc48eb0212457a08fa0a7eb4f5
Ramoo25 <Ramoo25@mail.ru>
2019-11-30 15:45:40 +0300

    Использование строк и столбцов упаковщика grid()

--- example.py
+++ example.py
@@ -7,12 +7,14 @@ def dump(*args):
 
 
 Butt1 = Button(root,text="Butt ON")
-Butt1.grid()
+Butt1.grid(row=0,column=0)
 Butt1.bind("<Button-1>",dump)
 
 Exit = Button(root,command=root.quit,text="Exit")
-Exit.grid()
+Exit.grid(row=0,column=1)
 
+Txt = Label(root,text="This is a label",bg="green",fg="red")
+Txt.grid(row=1,column=0)
 
 root.mainloop()
-root.destroy()
+#root.destroy()
