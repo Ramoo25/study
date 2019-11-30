277e3bd08e19b33f701c686d1ef076e4b6d8db72
Ramoo25 <Ramoo25@mail.ru>
2019-11-30 16:17:21 +0300

    Вес строк и столбцов grid()

--- example.py
+++ example.py
@@ -4,20 +4,21 @@ root = Tk()
 root.columnconfigure(0,weight = 1)
 root.columnconfigure(1,weight = 1)
 root.rowconfigure(0,weight = 1)
+root.rowconfigure(1,weight = 0)
 
 def dump(*args):
     print("DUMP:",*args)
 
 
 Butt1 = Button(root,text="Butt ON")
-Butt1.grid(row=0,column=0,sticky=E+W)
+Butt1.grid(row=0,column=0,sticky=E+W+S+N)
 Butt1.bind("<Button-1>",dump)
 
 Exit = Button(root,command=root.quit,text="Exit")
-Exit.grid(row=0,column=1,sticky=E+W)
+Exit.grid(row=0,column=1,sticky=E+W+S+N)
 
 Txt = Label(root,text="This is a label",bg="PeachPuff",fg="red")
-Txt.grid(row=1,column=0,columnspan=2,sticky=E+W)
+Txt.grid(row=1,column=0,columnspan=2,sticky=E+W+N)
 
 root.mainloop()
 #root.destroy()
